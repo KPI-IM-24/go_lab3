ccaf19f3f775ad325ff156b6d36dc1d3f8a5e385
sega <sergohack2015@gmail.com>
2024-06-02 20:32:20 +0300

    Implemented window with red T drawing

--- ui/window.go
+++ ui/window.go
@@ -16,6 +16,10 @@ import (
 	"golang.org/x/mobile/event/size"
 )
 
+const (
+	WindowSide = 800
+)
+
 type Visualizer struct {
 	Title         string
 	Debug         bool
@@ -25,15 +29,16 @@ type Visualizer struct {
 	tx   chan screen.Texture
 	done chan struct{}
 
-	sz  size.Event
-	pos image.Rectangle
+	sz     size.Event
+	center image.Point
 }
 
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
+	center := WindowSide / 2
+	pw.center.X = center
+	pw.center.Y = center
 	driver.Main(pw.run)
 }
 
@@ -43,7 +48,9 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  WindowSide,
+		Height: WindowSide,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -133,10 +140,19 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	pw.drawTFigure()
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
 		pw.w.Fill(br, color.White, draw.Src)
 	}
+}
+
+func (pw *Visualizer) drawTFigure() {
+	x, y := pw.center.X, pw.center.Y
+	red := color.RGBA{R: 255, G: 0, B: 0, A: 0} // Червоний колір
+
+	// Малювання T.
+	pw.w.Fill(image.Rect(x-60, y+140, x+60, y-140), red, draw.Src)
+	pw.w.Fill(image.Rect(x-150, y, x+150, y-140), red, draw.Src)
 }
