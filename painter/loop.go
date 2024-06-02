c398757a8114495b2e17d5745dbf14df79a7b518
Max <maksimhomisak64@gmail.com>
2024-06-02 18:08:59 +0200

    Initial commit
    clone base code from example repository
    initialize event loop

---
+++ painter/loop.go
@@ -0,0 +1,71 @@
+package painter
+
+import (
+	"image"
+
+	"golang.org/x/exp/shiny/screen"
+)
+
+// Receiver отримує текстуру, яка була підготовлена в результаті виконання команд у циклі подій.
+type Receiver interface {
+	Update(t screen.Texture)
+}
+
+// Loop реалізує цикл подій для формування текстури отриманої через виконання операцій отриманих з внутрішньої черги.
+type Loop struct {
+	Receiver Receiver
+
+	next screen.Texture // текстура, яка зараз формується
+	prev screen.Texture // текстура, яка була відправлення останнього разу у Receiver
+
+	mq messageQueue
+
+	stop    chan struct{}
+	stopReq bool
+}
+
+var size = image.Pt(400, 400)
+
+// Start запускає цикл подій. Цей метод потрібно запустити до того, як викликати на ньому будь-які інші методи.
+func (l *Loop) Start(s screen.Screen) {
+	l.next, _ = s.NewTexture(size)
+	l.prev, _ = s.NewTexture(size)
+
+	l.stop = make(chan struct{})
+	go func() {
+		for !l.stopReq || !l.mq.empty() {
+			message := l.mq.pull()
+			if update := message.Do(l.next); update {
+				l.Receiver.Update(l.next)
+				l.next, l.prev = l.prev, l.next
+			}
+		}
+		close(l.stop)
+	}()
+}
+
+// Post додає нову операцію у внутрішню чергу.
+func (l *Loop) Post(op Operation) {
+	l.mq.push(op)
+}
+
+// StopAndWait сигналізує про необхідність завершити цикл та блокується до моменту його повної зупинки.
+func (l *Loop) StopAndWait() {
+	l.Post(OperationFunc(func(t screen.Texture) {
+		l.stopReq = true
+	}))
+	<-l.stop
+}
+
+// TODO: Реалізувати чергу подій.
+type messageQueue struct{}
+
+func (mq *messageQueue) push(op Operation) {}
+
+func (mq *messageQueue) pull() Operation {
+	return nil
+}
+
+func (mq *messageQueue) empty() bool {
+	return false
+}
